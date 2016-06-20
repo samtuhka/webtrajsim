4f063848f90ae5e55fab5de2f1db47090b3b5be8
prinkkala <prinkkala@gmail.com>
2016-06-20 12:07:31 +0300

    added new launc window and new scenarios

--- circle.sh
+++ circle.sh
@@ -5,16 +5,16 @@ trap 'kill -HUP 0' EXIT
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 #BROWSER="chromium-browser --user-data-dir=chromium-data --allow-file-access-from-files"
-BROWSER="primusrun ./firefox/firefox/firefox"
-#BROWSER="primusrun ./chrome/full-build-linux/chrome \
-#	--test-type \
-#	--ignore-gpu-blacklist \
-#	--disable-setuid-sandbox \
-#	--disable-gpu-sandbox --user-data-dir=chromium-data \
-#	--allow-file-access-from-files \
-#	--start-fullscreen
-#	--js-flags=--expose-gc \
-#	--enable-precise-memory-info"
+#BROWSER="primusrun ./firefox/firefox/firefox"
+BROWSER="./chrome/chrome \
+	--test-type \
+	--ignore-gpu-blacklist \
+	--disable-setuid-sandbox \
+	--disable-gpu-sandbox --user-data-dir=chromium-data \
+	--allow-file-access-from-files \
+	--start-fullscreen
+	--js-flags=--expose-gc \
+	--enable-precise-memory-info"
 #BROWSER="primusrun chromium --test-type --ignore-gpu-blacklist --disable-setuid-sandbox --disable-gpu-sandbox --user-data-dir=chromium-data --allow-file-access-from-files"
 
 #SHOST=localhost
@@ -27,6 +27,6 @@ WPORT=10101
 #export vblank_mode=0
 #export __GL_FSAA_MODE=11
 #$BROWSER "http://$SHOST:$SPORT/index.html?controller=ws://$WHOST:$WPORT/"
-$BROWSER "file://$PWD/index.html?experiment=paavoDrivingAccelerate"
+$BROWSER "file://$PWD/index.html?disableDefaultLogger=true&experiment=paavoDrivingAccelerate"
 #$BROWSER "file://$PWD/index.html"
 #$BROWSER "http://$SHOST:$SPORT/index.html"
